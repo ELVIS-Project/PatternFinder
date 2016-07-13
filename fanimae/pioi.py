b8cb18a05703b55486cb0cb34f1205875a9a79fc
Marina Borsodi-Benson <marinaborsodibenson@gmail.com>
2016-07-13 14:44:21 -0400

    Added: function to calculate the total score

--- fanimae/pioi.py
+++ fanimae/pioi.py
@@ -1,7 +1,9 @@
 from Bio import pairwise2
-from Bio.pairwise2 import format_alignment
+# from Bio.pairwise2 import format_alignment
 import midiparser
 import os
+# from pprint import pprint
+import math
 
 
 def compare_pitch(p1, p2):
@@ -13,9 +15,7 @@ def compare_pitch(p1, p2):
     delete = -2
 
     alignment = pairwise2.align.localms(p1, p2, match, mismat, insert, delete)
-    for a in alignment:
-        print(format_alignment(*a))
-    return alignment
+    return alignment[0][2]
 
 
 def compare_ioi(ioi1, ioi2):
@@ -40,26 +40,41 @@ def compare_ioi(ioi1, ioi2):
     delete = -2
 
     alignment = pairwise2.align.localds(ioi1, ioi2, matrix, insert, delete)
-    for a in alignment:
-        print(format_alignment(*a))
-    return alignment
+    return alignment[0][2]
 
-query = 'example.xml'
-directory = 'midifiles'
 
-q_info = midiparser.process_file(query)
-q_info = q_info[0][0], q_info[1][0]
+def vector_score(score_dict):
+    '''Computing the total score based on the two parameters' scores'''
 
-d_num = {}
-for file in os.listdir(directory):
-    if file != '.DS_Store':
-        d_info = midiparser.process_file(directory + '/' + file)
-        dinfo = []
-        for n in range(len(d_info[0])):
-            dinfo.append((d_info[0][n], d_info[1][n]))
+    for piece in score_dict:
+        pitch, ioi = score_dict[piece]
+        pitch = math.pow(18, 2) * math.pow(pitch, 2)
+        ioi = math.pow(ioi, 2)
+        total = math.sqrt(pitch + ioi)
+        score_dict[piece] = total
+    return score_dict
 
-        for part in range(len(dinfo)):
-            # compare_ioi(q_info[0], dinfo[part][0])
-            # compare_pitch(q_info[1], dinfo[part][1])
-            compare_ioi('RlRRsLSsSslLL', dinfo[part][0])
-            compare_pitch('abcdefgabdfc', dinfo[part][1])
+
+def run(query, directory):
+
+    q_info = midiparser.process_file(query)
+    q_info = q_info[0][0], q_info[1][0]
+
+    d_num = {}
+    for file in os.listdir(directory):
+        if file != '.DS_Store':
+            d_info = midiparser.process_file(directory + '/' + file)
+            dinfo = []
+            for n in range(len(d_info[0])):
+                dinfo.append((d_info[0][n], d_info[1][n]))
+
+            for part in range(len(dinfo)):
+
+                ioi_score = compare_ioi(q_info[0], dinfo[part][0])
+                pitch_score = compare_pitch(q_info[1], dinfo[part][1])
+                d_num[file] = (pitch_score, ioi_score)
+                # compare_ioi('RlRRsLSsSslLL', dinfo[part][0])
+                # compare_pitch('abcdefgabdfc', dinfo[part][1])
+
+    final_scores = vector_score(d_num)
+    return final_scores
