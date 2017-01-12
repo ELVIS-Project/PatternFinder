a3e69537f54cbb1b429eda3e21169580d4c4439d
DavidGarfinkle <david.garfinkle@mail.mcgill.ca>
2017-01-12 14:46:48 -0500

    Tests: reorganized pange_lingua suite for easier test additions

--- c-brahms/tests/test_pange_lingua.py
+++ c-brahms/tests/test_pange_lingua.py
@@ -1,73 +1,60 @@
-from unittest import TestCase, TestLoader, TestSuite
-from nose_parameterized import parameterized, param
-from vis.analyzers.indexers import noterest, metre
-from tests import tools
-from functools import partial
-from LineSegment import TwoDVector, LineSegment
+from unittest import TestCase, TestLoader, TestSuite # test framework
+from nose_parameterized import parameterized, param # for auto-generating tests
+from vis.analyzers.indexers import noterest, metre # vis
+import music21
+import pandas
+from tests import tools # parsing tools
+from functools import partial # to force exact matching on all algorithms
+from LineSegment import TwoDVector, LineSegment # data structures
 import copy
 import cbrahmsGeo
 import midiparser
-import music21
-import pandas
 import pdb
 import nose
 
 
 class TestPangeLingua(TestCase):
 
-    P1 = partial(cbrahmsGeo.P1, option='onset')
-    P2 = partial(cbrahmsGeo.P2, option=0)
-    P3 = partial(cbrahmsGeo.P3, option=0)
-    algorithms=[
-        ("P1", P1),
-        ("P2", P2),
-        ("P3", P3)
-    ]
-
+    # Datapaths
     music_folder = "music_files/pange_lingua/"
     source_path = music_folder + "pange_lingua.xml"
 
     # Index the source for the whole class to avoid unnecessary re-indexing for each test case
     indexed_source = tools.index_source_from_score(source_path)
 
-    TESTS = [
-        # Tenor entry in measure 1. Repeats exactly in the Soprano at downbeat of m.5
-        {
-            'name' : "kyrie1.1_P1_tenor_entry",
-            'algorithm' : P1,
-            'query' : "music_files/pange_lingua/kyrie1.1_tenor1-4.xml",
-            'expected' : [[0, -12], [48, 0]]},
+    exact_algorithms=[
+        ("P1-onset", partial(cbrahmsGeo.P1, option='onset')),
+        ("P1-segment", partial(cbrahmsGeo.P1, option='segment')),
+        ("P2", partial(cbrahmsGeo.P2, option=0)),
+        ("P3", partial(cbrahmsGeo.P3, option=0))
+    ]
 
-        # Bass entry in measure 2. Repeats with 2 mismatch in the Alto at downbeat of m.6
-        {
-            'name' : "kyrie1.1_P2_bass_entry",
-            'algorithm' : P1,
-            'query' : music_folder + "kyrie1.1_bass2-5.xml",
-            'expected' : [[12, 0]]},
-        {
-            'name' : "kyrie1.1_P2_bass_entry_alto_imitation",
-            'algorithm' : partial(cbrahmsGeo.P2, option=2),
-            'query' : music_folder + "kyrie1.1_bass2-5.xml",
-            'expected' : [[60, 12]]}
-        ]
+    # List of queries. To add new tests - format is ("query_filename", [list of algorithms tuples e.g. ("algy-name", function) to test this query], [expected results])
+    QUERIES = [
+            # Tenor entry in measure 1. Repeats exactly in the Soprano at downbeat of m.5
+            ("kyrie1_tenor_mm1-4.xml", exact_algorithms, [[0, -12], [48, 0]]),
+            # Bass entry in measure 2. Repeats with 2 mismatch in the Alto at downbeat of m.6
+            ("kyrie1_bass_mm2-5.xml", exact_algorithms, [[12, 0]]),
+            ("kyrie1_bass_mm2-5.xml", [("P2-option2", partial(cbrahmsGeo.P2, option=2))], [[60, 12]])
+    ]
 
-    @parameterized.expand(param(**case) for case in TESTS)
-    def test(self, name, algorithm, query, expected, source=source_path):
+    TESTS = reduce(lambda x, y: x+y, [[{
+            'query' : music_folder + q,
+            'algorithm' : algy,
+            'expected' : e
+        } for algy in a] for (q, a, e) in QUERIES])
+
+    def custom_func_name(testcase_func, param_num, param):
+        return "%s_algorithm_%s_%s" % (
+                testcase_func.__name__,
+                param[1]['algorithm'][0], #algorithm[0] for string name
+                param[1]['query']
+        )
+
+    @parameterized.expand([param(**case) for case in TESTS], testcase_func_name = custom_func_name)
+    def test_pange_lingua(self, query, algorithm, expected, source=source_path):
         indexed_pattern = tools.index_pattern_from_score(query)
-        list_of_shifts = tools.run_algorithm_with_indexed_pieces(algorithm, indexed_pattern, TestPangeLingua.indexed_source)
+        list_of_shifts = tools.run_algorithm_with_indexed_pieces(algorithm[1], indexed_pattern, TestPangeLingua.indexed_source) #algorithm[1] for function pointer
         self.assertEqual(list_of_shifts, [TwoDVector(d[0], d[1]) for d in expected])
 
-"""
-        list_of_shifts = tools.run_algorithm_with_midiparser(algorithm, 'music_files/pange_lingua/kyrie1.1_tenor1-4.xml', 'music_files/pange_lingua/pange_lingua.xml')
-        self.assertEqual(list_of_shifts, [TwoDVector(0, -12), TwoDVector(48, 0)])
-
-        #find bass entry
-        list_of_shifts = tools.run_algorithm_with_midiparser(algorithm, 'music_files/pange_lingua/kyrie1.1_bass2-5.xml', 'music_files/pange_lingua/pange_lingua.xml')
-        self.assertEqual(list_of_shifts, [TwoDVector(12, 0)])
-
-        #find bass entry 2 mismatch in alto entry
-        list_of_shifts = tools.run_algorithm_with_midiparser(partial(cbrahmsGeo.P2, option=2), 'music_files/pange_lingua/kyrie1.1_bass2-5.xml', 'music_files/pange_lingua/pange_lingua.xml')
-        self.assertEqual(list_of_shifts, [TwoDVector(12, 60)])
-
-        """
 PANGE_LINGUA_SUITE = TestLoader().loadTestsFromTestCase(TestPangeLingua)
