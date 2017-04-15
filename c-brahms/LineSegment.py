d4b3e6de9c68037b4e8cd9db03f6ffc17f3b5b1f
DavidGarfinkle <david.garfinkle@mail.mcgill.ca>
2017-04-15 02:13:54 -0400

    New: Algorithm S2 online

--- c-brahms/LineSegment.py
+++ c-brahms/LineSegment.py
@@ -1,5 +1,7 @@
-import itertools
 from collections import namedtuple
+from copy import deepcopy # For use in LineSegmentSet so as not to modify the original segments when attaching indices
+import pdb
+import itertools
 
 Point = namedtuple('Point', ['x', 'y'])
 
@@ -15,6 +17,12 @@ class TwoDVector(object):
         elif self.vector() > other_vector.vector():
             return 1
         return 0
+
+    def __eq__(self, other_vector):
+        if self.vector() == other_vector.vector():
+            return True
+        else:
+            return False
 
     def __str__(self):
         return "{0}".format(str(self.vector()))
@@ -61,6 +69,7 @@ class Bucket():
         self.slope = slope
         self.prev_tp = prev_tp
 
+
 class LineSegment(TwoDVector):
     #TODO should be able to just add 4 to .x or .onset and everything updates
 
@@ -69,6 +78,7 @@ class LineSegment(TwoDVector):
         self.onset = onset
         self.pitch = pitch
         self.duration = duration
+        self.set_index = None
         self.update()
 #        self.turning_point = []
 
@@ -93,8 +103,7 @@ class LineSegment(TwoDVector):
     def __mul__(self, factor):
         """
         Custom mult to stretch or shrink the duration of a LineSegment
-        Expects a scalar value.
-        """
+        Expects a scalar value.  """
         return LineSegment(self.onset, self.pitch, self.duration * factor)
 
     def __div__(self, factor):
@@ -108,9 +117,15 @@ class LineSegment(TwoDVector):
         return super(LineSegment, self).__cmp__(other_line_segment)
 
     def __str__(self):
-        return "onset {0}, duration {1}, pitch {2}".format(self.onset, self.duration, self.pitch)
+        return "onset {0}, duration {1}, pitch {2}, set index {3}".format(self.onset, self.duration, self.pitch, self.set_index)
     def __repr__(self):
-        return "LineSegment(onset={0}, offset={1}, duration={2}, pitch={3})".format(self.onset, self.offset, self.duration, self.pitch)
+        msg = "LineSegment(onset={0}, pitch={1}, duration={2})"
+        # Include set indices in the repr
+        if self.set_index == None:
+            msg += "no set"
+        else:
+            msg = msg + "set index: " + self.set_index
+        return msg.format(self.onset, self.pitch, self.duration)
 
     def update(self):
         # update the offset
@@ -141,7 +156,28 @@ class LineSegment(TwoDVector):
         """
         return LineSegment(self.onset, self.pitch, other_line_segment.offset - self.onset)
 
+class Vector(TwoDVector):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.v = TwoDVector(end.x - start.x, end.y - start.y)
+
 class LineSegmentSet(list):
+
+    def __init__(self, segments):
+        copy_of_segments = deepcopy(segments)
+        self.extend(copy_of_segments)
+
+    def set_indices_of_segments(self):
+        for i in range(len(self)):
+            seg.set_index = i
+
+    def sort(key=None, reverse=None):
+        """
+        Sorts the list of segments and then reassigns indices to its constituents.
+        """
+        super(LineSegmentSet, self).sort(key, reverse)
+        self.set_indices_of_segments()
 
     def mergeOverlappingSegments(self):
         """
@@ -150,15 +186,6 @@ class LineSegmentSet(list):
             1) sorting the list by pitch and then onset
             2) zip it with itself offset by 1, map .doesOverlapWith, use groupby() and fold .mergeWith on the partitions
         """
-#        helper = zip(self, self[1:])
-#        map(lambda h: h[0].doesOverlapWith(h[1])
-#        then, partition the set with groupby() and use fold on the partitions
-#        new = [[la for x in la] for elt in self]
-
-#        for i in range(len(self[:-1])):
-#            while (i < len(self) - 1) and self[i].doesOverlapWith(self[i+1]):
-#                self[i] = self[i].mergeWith(self.pop(i+1))
-
         m = len(self)
         for i in range(m):
             j = i + 1
@@ -168,11 +195,104 @@ class LineSegmentSet(list):
                     m -= 1
                 j += 1
 
-#    def mergeOverlappingSegmentsNew(self):
-#        self.sort(key = lambda x: (x.pitch, x.onset))
-#        overlaps = [(e[0], e[1], e[0].doesOverlapWith[1]) for e in zip(self, self[1:])]
-        # Doesn't work cause it appends groups with 'False' also
-#        return [reduce(lambda l, r: l[0].mergeWith(r[0]), g) for g in groupby(overlaps, lambda x: x[2])]
+    def initialize_Ktables(self, S, source_window = 0, pattern_window = 1, start = 0):
+        """
+
+        By default, let pattern_window correspond to algorithm S1
+        """
+        # S souce LineSegmentSet, P pattern LineSegmentSet, w window
+        # each table K[i] stores all intra-database vectors (g) which match (g.y = f.y) any intra-pattern vectors (f) starting at p_i (i.e., p_i' - p_i for some i' > i) 
+        # so we can loop through all intra_pattern vectors and then find all database vectors which might match each one
+        # then use groupby() and dict.fromkeys to make a dictionary of possibly intra-database vectors, hashed to their corresponding g.y's, so it's easily searchable. for each f, go to the f.y bucket in the dict, and loop through the g's that could match it.
+
+        # TODO the last ktable shouldn't have entries? we can't have c == i??
+
+        # Compute all (n choose 2) intra-database vectors, then group them into dictionary buckets based on their g.y values
+        S.compute_intra_vectors(window = source_window, start = start)
+        self.compute_intra_vectors(window = pattern_window)
+
+        # TODO remove all of these [0]get items by implementing a Translation class which holds line segments which in turn hold set indices
+        keyfunc = lambda x: x[0].y
+        S.ivs_indices.sort(key=keyfunc)
+        intra_database_vectors = {key : list(g) for key, g in itertools.groupby(S.ivs_indices, keyfunc)}
+
+        self.K = []
+        for i in range(len(self)):
+            self.K.append([])
+            for j in range(i+1, min(i+1+pattern_window, len(self))): # j is denoted i' in the paper
+#            for j in [min(i+1, len(self)-1)]: # for S1 just consider p_{i+1} - p_{i}
+                f = self[j] - self[i]
+                # If there are no corresponding intra-database vectors, skip this iteration
+# TODO use groupby() and self.ivs so initialize_Ktables becomes algorithm-independent
+#        self.K = [[] for elt in self]
+#        for i in range(len(self)):
+#            f = self[i]
+                if not intra_database_vectors.has_key(f.y):
+                    continue
+                # Otherwise, add them to the table
+                for g in intra_database_vectors[f.y]: #TODO raise exception if f.y != g.y?
+                    ### TIME SCALING
+                    # "if both are zero, set s = 1"
+                    if f.x == 0 and g[0].x == 0:
+                        scale = 1
+                    # "if only one of them equals 0, or both are equal to infinity, delete the row"
+                    elif (f.x == float("inf") and g[0].x == float("inf")) or f.x == 0 or g[0].x == 0:
+                        continue
+                    else:
+                        scale = float(g[0].x) / f.x # TODO use fractions
+
+                    # Initialize the hth row of the ith K table
+                    #TODO make these vector objects, make TwoDVectors Point objects
+                    self.K[i].append({
+                        'a' : g[1], #store index of S[i]
+                        'b' : g[2], #store index of S[j]
+                        'y' : None,
+                        'c' : j, #index of P[j]
+                        's' : scale,
+                        'w' : 1,
+                        'z' : 0
+                    })
+            # Append the last row, denoted \sum{p_i} : the number of rows generated for table K[i]
+            self.K[i].append({
+                'a' : float("inf"),
+                'b' : float("inf"),
+                'c' : i + 1,
+                's' : 0,
+                'w' : 0,
+                # y, z are not initialize in the pseudocode but an KeyError is thrown in S2 without them
+                'y' : None,
+                'z' : 0
+            })
+            # Sort the K[i]th table in lexicographic order (id of point in source associated with p_i, scaling factor)
+            self.K[i].sort(key = lambda x : (x['a'], x['s']))
+
+
+    #TODO put this in the __init__ method? or an update method? etc.
+    # by default, window = len(self) so that if no window is set, compute all vectors
+    # use a sorted "self" since this should only take into account positive intra vectors
+    def compute_intra_vectors(self, window = 0, start = 0):
+        """
+        Given a LineSegmentSet, computes all (n choose 2) positive inner-translational vectors
+        Can accept a window to ignore vectors which point more than 'w' segments ahead in the list.
+        Also can indicate a 'start' point, to ignore any intra-vectors before a certain index.
+        Initializes the list of translations in self.ivs
+        """
+        # TODO make it so window = 0 means no window, so you don't need len(patterns) when you call this function
+        #TODO get rid of the "start" functionality? it was just to test lemstrom
+#        self.sort()
+#        self.ivs = [(self[j] - self[i]) for i in range(start, len(self)) for j in range(i + 1, min(i + 1 + window, len(self)))]
+
+        sorted_self = sorted(self)
+
+        # Find all non-duplicate pairs (up to some window) of intra-database vectors and sort them by their x translations.
+        # range(i+1, min(i+1 + window, len(self))) -- example: say w=3, len(self) > 23, and i=20. we should get vectors for 20->21, 20->22, 20->23
+        # this would evaluate to: for j in range(21, 21 + 3) --> [21, 22, 23]. Remember range(x,y) starts at 0 and ends at y-1 !!!
+
+        self.ivs_indices = sorted([(sorted_self[j] - sorted_self[i], i, j) for i in range(start, len(self)) for j in range(i+1, min(i+1 + window, len(self)))], key = lambda x: x[0].y)
+        self.ivs = sorted([sorted_self[j] - sorted_self[i] for i in range(start, len(self)) for j in range(i+1, min(i+1 + window, len(self)))], key = lambda x: x.y)
+
+        # Return hashed buckets where each unique x translation has its own bucket of intra-database vectors and corresponding indices
+#        self.ivs = {key : list(g) for key, g in itertools.groupby(intra_database_vectors, keyfunc)}
 
     def onsets_with_notes(self):
         return zip(self.onsets, self.notes)
