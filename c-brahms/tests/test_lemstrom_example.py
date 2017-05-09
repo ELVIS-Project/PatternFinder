706e5f4b20cedaaff4469b87e76d6c2234f791d6
DavidGarfinkle <david.garfinkle@mail.mcgill.ca>
2017-05-09 19:47:46 -0400

    Tests: Cleaned up lemstrom test generation

--- c-brahms/tests/test_lemstrom_example.py
+++ c-brahms/tests/test_lemstrom_example.py
@@ -4,53 +4,67 @@ from functools import partial
 from nose_parameterized import parameterized, param
 from LineSegment import TwoDVector, LineSegmentSet
 from pprint import pformat # for pretty printing test error messages
-from geometric_algorithms import P1, S1, S2, W1, W2
+from geometric_algorithms import P1, P2, P3, S1, S2, W1, W2
 from fractions import Fraction # for scale settings
 import music21
 import NoteSegment
-import cbrahmsGeo
-import midiparser
 import pdb
 
 
 lemstrom_pattern = lambda x: 'music_files/lemstrom2011_test/query_' + x + '.mid'
 lemstrom_score = 'music_files/lemstrom2011_test/leiermann.xml'
 
-algorithm_W = [("W1", W1), ("W2", W2)]
-algorithm_S = [("S1", S1), ("S2", S2)] + algorithm_W
-algorithm_P = algorithm_S + algorithm_W
-
 class TestLemstromExample(TestCase):
 
-    # List of queries. To add new tests - format is ("query_filename", [list of algorithms tuples e.g. ("algy-name", function) to test this query], [expected results])
-    QUERIES = [
-            ('a', (P1, S1, S2, W1, W2), (3.0, -10), {'scale' : 1, 'threshold' : 5}),
-            #('b', (S2, W2), (3.0, -10), {'scale' : 1, 'threshold' : 'max'), #figure out how to do max.. default is threshold = 2, but then there will be all sorts of matches returned along with the maximal occurrence
-            ('c', (S1, S2, W1, W2), (3.0, -10), {'scale' : Fraction(1, 3), 'threshold' : 5}),
-            #('d', (S2, W2), (3.0, -10), {'scale' : Fraction(1,3), 'threshold' : 'max'}),
-            ('e', (W1, W2), (3.0, -10), {'threshold' : 5})
-            #('f', (W2,), (3.0, -10))
-    ]
+    ## QUERIES in dict form. Each query is a 3-tuple consiting of an algorithm, the expected result, and settings to pass in.
+    # 'query_letter' : [(algorithm, [list of expected shifts], settings), ...]
+    #
+    # In exact matches, we restrict the error-tolerance of the algorithms:
+    # "scale" filters out found occurrences by how much they scaled the pattern
+    # "threshold" is the minimum number of notes that had to be found in any partial match
+    ##
+    QUERIES = {
+            ## Pure exact match
+            # P1 should find two duplicate occurrences since the first note (indices 12, 13) is duplicated (see behaviour of P1)
+            ##
+            'a' : [(P1, [(3.0, -10), (3.0, -10)], {})] + [(alg, [(3.0, -10)], {'scale' : 1, 'threshold' : 5}) for alg in (P2, P3, S1, S2, W1, W2)],
 
-    TESTS = reduce(lambda x, y: x+y, [[{ #concatenate the lists
-            'query' : q,
-            'algorithm' : algy,
-            'expected' : e
-        } for algy in a] for (q, a, e, s) in QUERIES])
+            ## Pure partial match with one mismatch
+            'b' : [(alg, [(3.0, -10)], {}) for alg in (P2, S2, W2)],
 
-    def custom_func_name(testcase_func, param_num, param):
+            ## Scaled exact match
+            'c' : [(alg, [(3.0, -10)], {'scale' : Fraction(1,3), 'threshold': 5}) for alg in (S1, S2, W1, W2)],
+
+            ## Scaled partial match with one mismatch
+            'd' : [(alg, [(3.0, -10)], {'scale' : Fraction(1,3), 'threshold': 5}) for alg in (S1, S2, W1, W2)],
+
+            ## Warped exact match
+            'e' : [(alg, [(3.0, -10)], {'threshold': 5}) for alg in (W1, W2)],
+
+            ## Warped partial match with one mismatch
+            'f' : [(alg, [(3.0, -10)], {'threshold': 5}) for alg in (W2,)]}
+
+    # Reduce QUERIES into list of tests
+    TESTS = []
+    for key, val in QUERIES.items():
+        for algorithm, list_of_shifts, settings in val:
+            TESTS.append((key, algorithm, list_of_shifts, settings))
+    # List comprehension wasn't working, no idea why.
+    #TESTS = [(key, algorithm, list_of_shifts, settings) for algorithm, list_of_shifts, settings in val for key, val in QUERIES.items()]
+
+    def custom_test_name(testcase_func, param_num, param):
         return "%s_algorithm_%s_Query_%s" % (
                 testcase_func.__name__,
-                param[1]['algorithm'].__name__, #algorithm[0] for string name
-                param[1]['query']
+                param.args[1].__name__, # Algorithm name
+                param.args[0] # Query name
         )
 
-    @parameterized.expand([param(**case) for case in TESTS], testcase_func_name = custom_func_name)
+    @parameterized.expand([param(*case) for case in TESTS], testcase_func_name = custom_test_name)
     def test(self, query, algorithm, expected, settings={}):
         carlos = algorithm(lemstrom_pattern(query), lemstrom_score, settings)
         carlos.run()
         #self.assertEqual(len(carlos.occurrences), len(expected))
-        self.assertEqual(carlos.occurrences[0].shift, expected)
+        self.assertEqual([x.shift for x in carlos.occurrences], expected)
 
     def test_intra_vectors_NOTESEGMENT(self):
         """
@@ -86,11 +100,11 @@ class TestLemstromExample(TestCase):
 
         # Sort the lists of vectors lexicographically
         result = sorted([(v.x, v.y) for v in source.ivs])
-        actual = sorted(lemstrom_vectors)
+        expected = sorted(lemstrom_vectors)
 
         # Assert and give an error message if false
         self.longMessage = True
-        self.assertListEqual(result, actual, msg = "\nRESULT\n" + pformat(list(enumerate(sorted(source.ivs)))) + "\nACTUAL\n" + pformat(list(enumerate(sorted(actual)))))
+        self.assertListEqual(result, expected, msg = "\nRESULT\n" + pformat(list(enumerate(sorted(source.ivs)))) + "\nACTUAL\n" + pformat(list(enumerate(sorted(expected)))))
 
     def test_intra_vectors_lemstrom(self):
         """
@@ -112,10 +126,10 @@ class TestLemstromExample(TestCase):
                 (0,9), (2,-1), (2,11), (2,-10), (2,2), (4,-12),
                 (0,12), (2,13), (0,0), (0,15), #(2,-14), (2,-2), # edited (0,1) -> (0,0)
                 (0,5), (0,12), (2,1), (2,3), (4,-14), (4,-2), (4,-1), (6,-7), (6,0)]] #These we add because Lemstrom's data seems to be incomplete in accordance to the musical excerpt
-        intended = sorted(source.ivs)
-        actual = sorted(lemstrom_vectors)
+        result = sorted(source.ivs)
+        expected = sorted(lemstrom_vectors)
         self.longMessage = True
-        self.assertListEqual(intended, actual, msg = "\nINTENDED\n" + pformat(list(enumerate(sorted(source.ivs_indices)))) + "\nACTUAL\n" + pformat(list(enumerate(sorted(actual)))))
+        self.assertListEqual(result, expected, msg = "\nINTENDED\n" + pformat(list(enumerate(sorted(source.ivs_indices)))) + "\nACTUAL\n" + pformat(list(enumerate(sorted(expected)))))
 
     def test_Ktable_lemstrom(self):
         pass
