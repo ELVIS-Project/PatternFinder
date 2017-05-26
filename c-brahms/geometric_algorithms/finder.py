c23e7eb3dd56d192ed0aee4d5734c3316f901697
DavidGarfinkle <david.garfinkle@mail.mcgill.ca>
2017-05-26 14:22:37 -0400

    Generalized finder class to choose algorithms from user settings

---
+++ c-brahms/geometric_algorithms/finder.py
@@ -0,0 +1,34 @@
+from geometric_algorithms.geo_algorithms import DEFAULT_SETTINGS
+import geometric_algorithms
+
+class Finder(object):
+    def __init__(self, pattern, source, **kwargs):
+        # Update the default settings with user-specified ones so that the user only has to specify non-default parameters.
+        settings = {key : val for key, val in DEFAULT_SETTINGS.items()}
+        settings.update(kwargs)
+
+        if settings['scale'] == 1 or settings['scale'] == 'pure':
+            cls = 'P'
+        elif settings['scale'] == 'warped':
+            cls = 'W'
+        else:
+            cls = 'S'
+
+        if settings['threshold'] == 'all':
+            tp = '1'
+        else:
+            tp = '2'
+
+        if settings['algorithm']:
+            algorithm_name = settings['algorithm']
+        else:
+            algorithm_name = cls + tp
+
+        algorithm = getattr(geometric_algorithms, algorithm_name)
+        self.algorithm = algorithm(pattern, source, **kwargs)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.algorithm.next()
