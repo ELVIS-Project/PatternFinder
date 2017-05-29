64e4b371ebde3f4a18c1bbc28c923ef2fefa987a
DavidGarfinkle <david.garfinkle@mail.mcgill.ca>
2017-05-29 17:24:59 -0400

    Renamed requirements, updated run_tests

--- c-brahms/run_tests.py
+++ c-brahms/run_tests.py
@@ -1,12 +1,13 @@
 from unittest import TextTestRunner
-from tests import test_P1, test_P2, test_lemstrom_example, test_exact_matches, test_pange_lingua
+#from tests import test_P1, test_P2, test_lemstrom_example, test_exact_matches, test_pange_lingua
+from tests import test_lemstrom_example
 
 tests = [
-        (1, test_lemstrom_example.LEMSTROM_EXAMPLE_SUITE),
-        (2, test_exact_matches.EXACT_MATCHES_SUITE),
-        (1, test_pange_lingua.PANGE_LINGUA_SUITE),
+        (1, test_lemstrom_example.LEMSTROM_EXAMPLE_SUITE)
+        #(2, test_exact_matches.EXACT_MATCHES_SUITE),
+        #(1, test_pange_lingua.PANGE_LINGUA_SUITE),
         #(1, test_P1.P1_SUITE),
-        (1, test_P2.P2_SUITE)
+        #(1, test_P2.P2_SUITE)
     ]
 
 for test in tests:
