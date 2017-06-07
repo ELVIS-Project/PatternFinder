0a5af2e61a745222f836ecb6291260b3d7956627
DavidGarfinkle <david.garfinkle@mail.mcgill.ca>
2017-06-07 12:42:48 -0400

    New project directory structure! Nice and shiny

---
+++ c-brahms/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='PatternFinder',
+        version='0.1dev',
+        description='Polyphonic Music Pattern Finder',
+        author='David Garfinkle',
+        author_email='david.garfinkle@mail.mcgill.ca',
+        url='https://github.com/ELVIS-Project/VIS-Ohrwurm/',
+        packages=['geometric_algorithms']
+        )
