8752a99d0275a3e6bce925fafc397a7ab34deba7
DavidGarfinkle <david.garfinkle@mail.mcgill.ca>
2017-07-31 17:07:19 -0400

    Use suites taken from test init files

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,15 +1,11 @@
-from unittest import TextTestRunner
-#from tests import test_P1, test_P2, test_lemstrom_example, test_exact_matches, test_pange_lingua
-from tests import test_lemstrom_example
+import unittest
+import path
+from patternfinder.geometric_helsinki.tests import geometric_helsinki_suite
 
 tests = [
-        (2, test_lemstrom_example.LEMSTROM_EXAMPLE_SUITE)
-        #(2, test_exact_matches.EXACT_MATCHES_SUITE),
-        #(1, test_pange_lingua.PANGE_LINGUA_SUITE),
-        #(1, test_P1.P1_SUITE),
-        #(1, test_P2.P2_SUITE)
+        (2, geometric_helsinki_suite)
     ]
 
 for test in tests:
-    result = TextTestRunner(verbosity=test[0]).run(test[1])
+    result = unittest.TextTestRunner(verbosity=test[0]).run(test[1])
 
