5a0ae5d863385af3d9714ff9a9bd9f29780affe1
DavidGarfinkle <david.garfinkle@mail.mcgill.ca>
2017-08-01 14:58:09 -0400

    Moved test suite creation to geometric_helsinki/__init__

--- patternfinder/geometric_helsinki/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from test_lemstrom_example import lemstrom_example_suite
-from test_finder import finder_suite
-
-geometric_helsinki_suite = unittest.TestSuite([
-    lemstrom_example_suite,
-    finder_suite])
-
