289e04ca210e43ffe7da904f289bd0f46738906d
DavidGarfinkle <david.garfinkle@mail.mcgill.ca>
2017-08-02 17:19:22 -0400

    Added doctests and cleaned up import statements

--- patternfinder/geometric_helsinki/finder.py
+++ patternfinder/geometric_helsinki/finder.py
@@ -1,87 +1,105 @@
-from builtins import object # for python 2 & 3 custom iterator compatibility
-from patternfinder.geometric_helsinki.NoteSegment import NotePointSet, CmpItQueue
-from pprint import pformat # for logging
-from fractions import Fraction # for scale verification
-#from geometric_helsinki import DEFAULT_SETTINGS
-#from geometric_helsinki.settings import DEFAULT_SETTINGS
-from collections import namedtuple # for use in __repr__
-from patternfinder.geometric_helsinki.occurrence import Occurrence
-import algorithms
-import copy # to copy excerpts from the score
-import music21
-import patternfinder.geometric_helsinki # Why isn't geometric_helsinki already present at top level namespace if Finder is imported?
-import logging
-import logging.config
 import os
 import yaml
+import logging
 import pdb
 
-DEFAULT_SETTINGS = {
-        'algorithm' : 'P1',
-        'pattern_window' : 1,
-        'source_window' : 5,
-        'scale' : 'pure',
-        'threshold' : 'all',
-        'mismatches' : 0,
-        'interval_func' : 'semitones',
-        'colour' : 'red',
-        'modify_source' : False,
-        'show_pattern' : False,
-        'excerpt' : True,
-        'auto_select' : True,
-        'pattern' : None,
-        'source' : None,
-        'load_defaults' : False}
+import music21
 
+from pprint import pformat # for logging
+from builtins import object # for python 2 & 3 custom iterator compatibility
+from fractions import Fraction # for scale verification
+from collections import namedtuple # for use in __repr__
 
-class ValidationError(Exception):
-    def __init__(self, msg, key, arg, valid_options):
-        self.message = "\n".join([
-            msg + " \n",
-            "Parameter '{0}' has value of {1}".format(key, arg),
-            "Valid arguments are: {0}".format(e.message)])
+#import patternfinder.geometric_helsinki.algorithms
+from patternfinder.geometric_helsinki.algorithms import GeometricHelsinkiBaseAlgorithm
+from patternfinder.geometric_helsinki.GeometricNotes import NotePointSet
+from patternfinder.geometric_helsinki.occurrence import Occurrence
+
+## SETTINGS
+DEFAULT_SETTINGS_PATH = 'patternfinder/geometric_helsinki/default_settings.yaml'
+
+## Load default settings
+if os.path.exists(DEFAULT_SETTINGS_PATH):
+    with open(DEFAULT_SETTINGS_PATH, 'rt') as f:
+        DEFAULT_SETTINGS = yaml.safe_load(f.read())
+else:
+    logging.getLogger(__name__).warning(DEFAULT_SETTINGS_PATH + "not found; we will use the hard-coded"
+            + "dictionary stored in " + __name__ + " to determine the default settings")
+    DEFAULT_SETTINGS = {
+            'algorithm' : 'auto',
+            'pattern_window' : 1,
+            'source_window' : 5,
+            'scale' : 'pure',
+            'threshold' : 'all',
+            'mismatches' : 0,
+            'interval_func' : 'semitones',
+            'colour' : 'red',
+            'modify_source' : False,
+            'show_pattern' : False,
+            'excerpt' : True,
+            'auto_select' : True,
+            'pattern' : None,
+            'source' : None,
+            'load_defaults' : False}
 
 class Finder(object):
     """
-    Desired syntax:
+    A python generator responsible for the execution of geometric helsinki algorithms
 
-        Finder()
-        Finder.update(pattern='foo.krn', source='bar.krn') or
-        Finder.update(foo.krn, bar.krn)
+    Doctests
+    ---------
+    >>> import music21
+    >>> import patternfinder.geometric_helsinki as helsinki
+    >>> my_finder = helsinki.Finder()
 
-        Finder('pattern.krn', 'source.krn')
+    >>> p = music21.converter.parse('tinynotation: 4/4 c4 d4 e2')
+    >>> my_finder.update(pattern=p)
 
-        Finder(source='bar.krn')
-        Finder.update(pattern='foo.krn')
+    >>> s = music21.converter.parse('tinynotation: 4/4 c4 d4 e2')
+    >>> my_finder.update(source=s)
 
-        When either source or pattern are unspecified, they should be NoneType, not a Stream
-
-        @TODO update is broken for pattern / source changing. certain settings must be recalculated such as threshold and pattern window. maybe make these things functions of the length?
-        @TODO put occurrences in a separate occurrence class, so we can do:
-            for occ in finder:
-                occ.colour_in_score()
-        @TODO no measure info runs into errors because of occ excerpt writing. check for measure info!
-        @TODO Delay creating the algorithm (or even pre processing) until you have both the pattern and the source! what's the point while one is None?
-        @TODO P2 is broken - cannot find modules A1, A2, B1, and B2 in the Palestrina Kyrie movement
-        @TODO with pointers, you can put a progress on the algorithm completion!
+    >>> next(my_finder).offset
+    0.0
     """
     def __init__(self, pattern=None, source=None, **kwargs):
         """
-        A Finder object is like an algorithm factory.
-        Taking into account user-specified settings, it picks an algorithm
-        and returns a generator to loop through all occurrences
-        of the pattern within the source.
+        Input (optional - can be initialized with nothing)
+        ------
+        pattern: the query for which we are looking for in the source
+            str filename pointing to a symbolic music file
+            music21.stream.Stream
+        source: the database of music in which we are looking for the pattern
+            str filename pointing to a symbolic music file
+            music21.stream.Stream
+        settings: keyword arguments which choose the algorithm and manage its execution
+            keyword arguments (like an unpacked dictionary)
 
+        Output
+        ------
+        python generator which yields Occurrence objects
+
+        >>> import music21
+        >>> import patternfinder.geometric_helsinki as helsinki
         >>> p = music21.converter.parse('tinynotation: 4/4 c4 e4')
-        >>> s = music21.converter.parse('tinynotation: 4/4 c4 e4 c2 e2')
-        >>> foo = Finder(p, s)
-        >>> occ = next(foo) # occ is a list of InterNoteVectors
-        >>> (occ[0].x, occ[0].y)
-        (0.0, 0)
+        >>> s = music21.converter.parse('tinynotation: 4/4 c4 e4 r2 c2 e2 c2 r2 e1')
+        >>> my_finder = helsinki.Finder(p, s)
+        >>> occ = next(my_finder) # occ is an Occurrence object
+        >>> occ.measure_range
+        [1]
 
-        >>> foo.update(scale=0.5)
-        >>> next(foo)
+        >>> my_finder.update(scale=2)
+        >>> next(my_finder).measure_range
+        [2]
 
+        >>> my_finder.update(scale='warped')
+        >>> for occ in my_finder:
+        ...     occ.measure_range
+        [1]
+        [1, 2]
+        [1, 2, 3]
+        [2]
+        [2, 3, 4]
+        [3, 4]
         """
         # Log creation of this object
         self.logger = logging.getLogger(__name__)
@@ -318,13 +336,25 @@ class Finder(object):
         """
         Validates and translates the 'algorithm' parameter
         """
-        valid_options = ['P1', 'P2', 'P3', 'S1', 'S2', 'W1', 'W2']
+        valid_options = ['P1', 'P2', 'P3', 'S1', 'S2', 'W1', 'W2', 'auto']
         if arg in valid_options:
             return arg
         else:
             raise ValueError(valid_options)
 
     def _validate_threshold(self, arg):
+        """
+        Threshold defines the number of pattern notes which must be incorporated
+        into any returned occurrence
+
+        Options:
+            'all'
+                every pattern note has to be found
+            int x
+                at least x notes must be found
+            0 < p < 1
+                at least p (as a percentage) notes must be found
+        """
         valid_options = []
 
         valid_options.append('all')
@@ -413,82 +443,20 @@ class Finder(object):
             raise ValueError(valid_options.keys())
 
     def _validate_colour(self, arg):
-        # @TODO validate colour
+        # @TODO validate colour - check valid string values
+        # xml takes hexadecimal colours, lilypond does not
         valid_options = ['any hexadecimal RGB colour?']
         return arg
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    """
-    1) THRESHOLD AND MISMATCHES
-
-    # User-settings limitations (these limitations don't apply to default settings)
-    # @TODO make threshold and mismatches define an upper/lower bound range of tolerance
-    if ('threshold' in kwargs) and ('mismatches' in kwargs):
-        raise ValueError("Threshold and mismatches not yet both supported: use one or the other")
-
-
-    2) PARSE SCORES
-
-    update()
-    ### PARSE SCORES
-    if 'pattern' in kwargs:
-        self.pattern = self.parse_score(kwargs['pattern'])
-        logger.debug("Parsed pattern")
-        self.patternPointSet = NotePointSet(self.pattern)
-        logger.debug("NotePointSet: Sorted and chord-flattened pattern")
-    if 'source' in kwargs:
-        self.source = self.parse_score(kwargs['source'])
-        logger.debug("Parsed source")
-        self.sourcePointSet = NotePointSet(self.source)
-        logger.debug("NotePointSet: Sorted and chord-flattened source")
-
-
-    def parse_score(self, file_or_stream):
-        The input to Finder can be a symbolic music file or a music21 Stream
-
-        We check before leaping rather than duck typing because the exceptions
-        thrown by music21.converter.parse vary widely over many possible inputs
-
-        Raises ValueError if input is neither an instance of str nor music21.stream.Stream
-        logger = logging.getLogger("{0}.{1}".format(__name__, 'parse_scores'))
-
-        # Check before you leap
-        if isinstance(file_or_stream, str):
-            score = music21.converter.parse(file_or_stream)
-            score.derivation.origin = music21.ElementWrapper(file_or_stream)
-            score.derivation.method = 'music21.converter.parse()'
-        elif isinstance(file_or_stream, music21.stream.Stream):
-            score = file_or_stream
-            score.derivation.method = 'user input'
-        # Allow for pattern or source to be None, which is the default value in Finder __init__()
-        elif not file_or_stream:
-            return
-        else:
-            raise ValueError("Invalid score input!"
-                    + "{0} must be a file name (string) or music21 Stream.".format(file_or_stream))
-        return score
-
-    """
-
+""""
+Not used but would potentially make the code a lot cleaner
+class ValidationError(Exception):
+    def __init__(self, msg, key, arg, valid_options):
+        self.message = "\n".join([
+            msg + " \n",
+            "Parameter '{0}' has value of {1}".format(key, arg),
+            "Valid arguments are: {0}".format(e.message)])
+"""
 
 if __name__ == "__main__":
     import doctest
