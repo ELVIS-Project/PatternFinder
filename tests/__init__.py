cc2f1d60d03bdf357b3d5e0b8a472964caa75ae0
DavidGarfinkle <david.garfinkle@mail.mcgill.ca>
2017-12-03 15:14:21 -0500

    Tests: functions to run from IPython console

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,27 @@
 import unittest
+import testtools
 import path
-from patternfinder.geometric_helsinki.tests import geometric_helsinki_suite
 
-tests = [
-        (2, geometric_helsinki_suite)
+from patternfinder.geometric_helsinki import *
+
+from test_exact_matches import exact_matches_suite
+from geometric_helsinki import geometric_helsinki_suite
+
+testrunner = unittest.TextTestRunner(verbosity=2)
+
+suites = [
+        (2, geometric_helsinki_suite),
+        (2, exact_matches_suite)
     ]
 
-for test in tests:
-    result = unittest.TextTestRunner(verbosity=test[0]).run(test[1])
+def run_all():
+    for test in suites:
+        result = unittest.TextTestRunner(verbosity=test[0]).run(test[1])
 
+
+__all__ = [
+        'run_all',
+        'testrunner',
+        'geometric_helsinki_suite',
+        'exact_matches_suite'
+        ]
