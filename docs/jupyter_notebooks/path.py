f830180e28d6b8df64e32f2a0db4c9ba3b0b076c
DavidGarfinkle <david.garfinkle@mail.mcgill.ca>
2018-01-09 16:39:41 -0500

    New: Jupyter Tutorials

--- docs/jupyter_notebooks/path.py
+++ docs/jupyter_notebooks/path.py
@@ -1,4 +1,16 @@
 import sys
 import os
+import music21
 #sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '/home/dgarfinkle/PatternFinder')))
 os.chdir('/home/dgarfinkle/PatternFinder/')
+
+os.environ['QT_QPA_PLATFORM'] = 'offscreen'
+
+us = music21.environment.UserSettings()
+us['musicxmlPath'] = '/usr/bin/musescore'
+us['musescoreDirectPNGPath'] = '/usr/bin/musescore'
+us['ipythonShowFormat'] = 'ipython.musicxml.png'
+us['showFormat'] = 'musicxml'
+us['pdfPath'] = '/usr/bin/evince'
+us['lilypondPath'] = '/usr/bin/lilypond'
+
