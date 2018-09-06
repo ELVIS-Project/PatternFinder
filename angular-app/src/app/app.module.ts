05866824476f647fbf7c224ec865d93fb4cf466c
DavidGarfinkle <david.garfinkle@mail.mcgill.ca>
2018-09-06 18:37:11 -0400

    New: include @ng-bootstrap/ng-bootstrap

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { ExcerptComponent } from './excerpt/excerpt.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExcerptComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
